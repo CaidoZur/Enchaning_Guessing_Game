3c5a3e83ed871e17b713cfa06e3f5bf2ba43205e
CaidoZur <officialzurgmon7@gmail.com>
2024-04-29 17:30:36 +0800

    Changes 1

--- Client.py
+++ Client.py
@@ -1,44 +1,27 @@
 import socket
 
-host = "127.0.0.1"
+
+host = "10.125.41.154"
 port = 7777
 
-def get_difficulty():
-    while True:
-        difficulty = input("Choose difficulty (easy, medium, hard): ").lower()
-        if difficulty in ["easy", "medium", "hard"]:
-            return difficulty
-        else:
-            print("Invalid difficulty. Please choose again.")
 
 s = socket.socket()
 s.connect((host, port))
 
-# Receive the banner
+# received the banner
 data = s.recv(1024)
-# Print banner
+# print banner
 print(data.decode().strip())
 
 while True:
-    # Get user's name
-    name = input("Enter your name: ").strip()
-    s.sendall(name.encode())
-    # Get difficulty level from user
-    difficulty = get_difficulty()
-    s.sendall(difficulty.encode())
+    #let get our input from the user
+    user_input = input("").strip()
 
-    while True:
-        user_input = input("").strip()
-        s.sendall(user_input.encode())
-        reply = s.recv(1024).decode().strip()
-        if "Correct" in reply:
-            print(reply)
-            break
+    s.sendall(user_input.encode())
+    reply = s.recv(1024).decode().strip()
+    if "Correct" in reply:
         print(reply)
-        continue
-    
-    play_again = input("Do you want to play again? (yes/no): ").strip().lower()
-    if play_again != "yes":
         break
-
+    print(reply)
+    continue
 s.close()
